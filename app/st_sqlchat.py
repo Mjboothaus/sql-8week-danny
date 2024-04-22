476be2076d89d7fa6b5d965c6223d26cf09869b5
Michael Booth <michael@databooth.com.au>
2024-04-22 15:31:15 +1000

    Prog - create generic version

--- app/st_sqlchat.py
+++ app/st_sqlchat.py
@@ -6,28 +6,30 @@ import streamlit as st
 from pandas import Timestamp
 from sql_8week_danny.sql_engine import DuckDBEngine
 
-WEEK1_SQL = Path.cwd() / "sql/week1.sql"
-db = DuckDBEngine()
-db.execute_sql_file(WEEK1_SQL)
-
-st.sidebar.markdown(f"## Data with Danny - {WEEK1_SQL.stem.capitalize()}")
+import tomllib
+from pathlib import Path
+from loguru import logger
 
 
-# query_input = st.sidebar.text_input("Enter your SQL query here:", "")
-
-limit = st.sidebar.number_input("Limit rows:", min_value=1, value=10, step=1)
-
-
-# UI for choosing input location is not needed for chat_input
-# Removed for simplicity in this context
-
-# Maintain a session-level chat history
-if "chat_history" not in st.session_state:
-    st.session_state.chat_history = []
+def read_config():
+    config_toml = Path.cwd() / "app" / "app.toml"
+    if not config_toml.exists():
+        logger.error(f"File Not Found - {str(config_toml)}")
+        raise FileNotFoundError(f"{str(config_toml)}")
+    with open(config_toml, "rb") as f:
+        logger.info(f"Config loaded from {str(config_toml)}")
+        return tomllib.load(f)
 
 
-# Function to handle query execution and response
-def handle_query(query):
+def setup_db(config):
+    CREATE_SQL = Path.cwd() / config["sql"]["create_sql"]
+    assert CREATE_SQL.exists()
+    db = DuckDBEngine()
+    db.execute_sql_file(CREATE_SQL)
+    return db
+
+
+def handle_query(db, query):
     if query.lower().startswith("select"):
         result = db.query(query)
         if isinstance(result, pd.DataFrame):
@@ -39,48 +41,22 @@ def handle_query(query):
         else:
             return "No results returned."
     else:
-        return "Only SELECT queries are allowed."
+        return "Only SELECT based queries are allowed."
 
 
 def add_response_to_chat_history(response):
     # if isinstance(response, pd.DataFrame):
     #    # Convert DataFrame to a list of records for JSON serialization
-    #    serializable_response = response.to_dict('records')
+    #    serial_response = response.to_dict('records')
     # else:
-    serializable_response = response
-    st.session_state.chat_history.append({"DuckDB": serializable_response})
+    serial_response = response
+    st.session_state.chat_history.append({config["app"]["response_name"]: serial_response})
 
 
-# Chat input for SQL queries
-user_query = st.chat_input("Enter your SQL query:", key="chat_input")
-
-# Process and display chat history
-if user_query:
-    # Add user query to chat history
-    st.session_state.chat_history.append({"User": user_query})
-    # Execute query and get response
-    response = handle_query(user_query)
-    # Add response to chat history
-    add_response_to_chat_history(response)
-
-# Display chat history
-for chat in st.session_state.chat_history:
-    if "User" in chat:
-        st.caption("User:")
-        st.code(f"{chat['User']}")
-    elif "DuckDB" in chat:
-        st.caption("Result:")
-        st.write(chat["DuckDB"])
-        st.markdown("----")
-
-# Note: Downloading chat history as a file is omitted for brevity
-# You can add similar functionality as shown previously if needed
-
-
-def convert_chat_history_to_serializable(chat_history):
-    serializable_history = []
+def convert_chat_history_to_serial(chat_history):
+    serial_history = []
     for entry in chat_history:
-        serializable_entry = {}
+        serial_entry = {}
         for key, value in entry.items():
             # Check if the value is a DataFrame directly
             if isinstance(value, pd.DataFrame):
@@ -95,44 +71,79 @@ def convert_chat_history_to_serializable(chat_history):
                     {k: v.isoformat() if isinstance(v, pd.Timestamp) else v for k, v in record.items()}
                     for record in value
                 ]
-            serializable_entry[key] = value
-        serializable_history.append(serializable_entry)
-    return serializable_history
+            serial_entry[key] = value
+        serial_history.append(serial_entry)
+    return serial_history
 
 
 def get_sql_queries_only(chat_history):
-    sql_queries = [entry["User"] for entry in chat_history if "User" in entry]
+    sql_queries = [
+        entry[config["app"]["prompt_name"]]
+        for entry in chat_history
+        if config["app"]["prompt_name"] in entry
+    ]
     return sql_queries
 
 
-sql_queries = get_sql_queries_only(st.session_state.chat_history)
-sql_queries_str = "\n".join(sql_queries)
+def default_handler(x):
+    if isinstance(x, pd.Timestamp):
+        return x.isoformat()
+    raise TypeError(f"Object of type {type(x).__name__} is not JSON serial")
 
-# Assuming st.session_state.chat_history is already populated, including DataFrames
+
+config = read_config()
+db = setup_db(config)
+
+# Create UI
+
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+st.sidebar.markdown(f"## {config['app']['title']}")
+
+user_query = st.chat_input("Enter your SQL query:", key="chat_input")
+
+# TODO: Validate/check the query
+
+
+if user_query:
+    st.session_state.chat_history.append({config["app"]["prompt_name"]: user_query})
+    response = handle_query(db, user_query)
+    add_response_to_chat_history(response)
+
+for chat in st.session_state.chat_history:
+    if config["app"]["prompt_name"] in chat:
+        st.caption(f"{config['app']['prompt_name']}:")
+        st.code(f"{chat[config['app']['prompt_name']]}")
+    elif config["app"]["response_name"] in chat:
+        st.caption(f"{config['app']['response_name']}:")
+        st.write(chat[config["app"]["response_name"]])
+        st.markdown("----")
+
+
+sql_limit = st.sidebar.number_input("Limit rows:", min_value=1, value=10, step=1)
+
 
 if st.sidebar.button("Download Query History"):
-    serializable_chat_history = convert_chat_history_to_serializable(st.session_state.chat_history)
+    serial_chat_history = convert_chat_history_to_serial(st.session_state.chat_history)
 
-    st.write(serializable_chat_history)
-    # Serialize the serializable chat history to JSON
+    st.write(serial_chat_history)
 
-    def default_handler(x):
-        if isinstance(x, pd.Timestamp):
-            return x.isoformat()
-        raise TypeError(f"Object of type {type(x).__name__} is not JSON serializable")
-
-    chat_history_json = json.dumps(serializable_chat_history, indent=4, default=default_handler)
+    chat_history_json = json.dumps(serial_chat_history, indent=4, default=default_handler)
 
     # Create a download button for the JSON file
     st.sidebar.download_button(
-        label="Download Chat History",
+        label="Download: Chat History",
         data=chat_history_json,
         file_name="chat_history.json",
         mime="application/json",
     )
 
+    sql_queries = get_sql_queries_only(st.session_state.chat_history)
+    sql_queries_str = "\n".join(sql_queries)
+
     st.sidebar.download_button(
-        label="Download SQL Queries only",
+        label="Download: SQL Queries only",
         data=sql_queries_str,
         file_name="sql_queries.sql",
         mime="text/plain",
