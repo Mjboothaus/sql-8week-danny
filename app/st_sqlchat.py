0ffff1b7e1a676a567b2d712e4aec00c7b23c3b0
Michael Booth <michael@databooth.com.au>
2024-04-23 10:10:33 +1000

    Progress

--- app/st_sqlchat.py
+++ app/st_sqlchat.py
@@ -21,16 +21,16 @@ def read_config():
         return tomllib.load(f)
 
 
-def setup_db(config):
+def setup_DuckDB(config):
     CREATE_SQL = Path.cwd() / config["sql"]["create_sql"]
-    assert CREATE_SQL.exists()
-    db = DuckDBEngine()
+    db = DuckDBEngine(config)
     db.execute_sql_file(CREATE_SQL)
     return db
 
 
 def handle_query(db, query):
-    if query.lower().startswith("select"):
+    validate = db.validate(query)
+    if validate[0]:  # TODO: Wire in validation of SQL
         result = db.query(query)
         if isinstance(result, pd.DataFrame):
             # For DataFrame, convert to markdown for chat display
@@ -41,24 +41,23 @@ def handle_query(db, query):
         else:
             return "No results returned."
     else:
-        return "Only SELECT based queries are allowed."
+        return validate[1]
 
 
-def add_response_to_chat_history(response):
+def add_response_to_chat_hist(response):
     # if isinstance(response, pd.DataFrame):
     #    # Convert DataFrame to a list of records for JSON serialization
     #    serial_response = response.to_dict('records')
     # else:
     serial_response = response
-    st.session_state.chat_history.append({config["app"]["response_name"]: serial_response})
+    st.session_state.chat_hist.append({config["app"]["response_name"]: serial_response})
 
 
-def convert_chat_history_to_serial(chat_history):
+def convert_chat_hist_to_serial(chat_hist):
     serial_history = []
-    for entry in chat_history:
+    for entry in chat_hist:
         serial_entry = {}
         for key, value in entry.items():
-            # Check if the value is a DataFrame directly
             if isinstance(value, pd.DataFrame):
                 # Convert DataFrame to a list of records (dictionaries)
                 value = value.map(lambda x: x.isoformat() if isinstance(x, pd.Timestamp) else x).to_dict(
@@ -76,10 +75,10 @@ def convert_chat_history_to_serial(chat_history):
     return serial_history
 
 
-def get_sql_queries_only(chat_history):
+def get_sql_queries_only(chat_hist):
     sql_queries = [
         entry[config["app"]["prompt_name"]]
-        for entry in chat_history
+        for entry in chat_hist
         if config["app"]["prompt_name"] in entry
     ]
     return sql_queries
@@ -92,26 +91,31 @@ def default_handler(x):
 
 
 config = read_config()
-db = setup_db(config)
+db = setup_DuckDB(config)
+
+st.sidebar.markdown("### Tables:")
+for table in db.table_names:
+    st.sidebar.button(label=table, use_container_width=True, help=table)
+
 
 # Create UI
 
-if "chat_history" not in st.session_state:
-    st.session_state.chat_history = []
+if "chat_hist" not in st.session_state:
+    st.session_state.chat_hist = []
 
 st.sidebar.markdown(f"## {config['app']['title']}")
 
 user_query = st.chat_input("Enter your SQL query:", key="chat_input")
 
-# TODO: Validate/check the query
+# TODO: Validate/check the query (prob in handle_response)
 
 
 if user_query:
-    st.session_state.chat_history.append({config["app"]["prompt_name"]: user_query})
+    st.session_state.chat_hist.append({config["app"]["prompt_name"]: user_query})
     response = handle_query(db, user_query)
-    add_response_to_chat_history(response)
+    add_response_to_chat_hist(response)
 
-for chat in st.session_state.chat_history:
+for chat in st.session_state.chat_hist:
     if config["app"]["prompt_name"] in chat:
         st.caption(f"{config['app']['prompt_name']}:")
         st.code(f"{chat[config['app']['prompt_name']]}")
@@ -124,22 +128,21 @@ for chat in st.session_state.chat_history:
 sql_limit = st.sidebar.number_input("Limit rows:", min_value=1, value=10, step=1)
 
 
+# Download buttons
+
 if st.sidebar.button("Download Query History"):
-    serial_chat_history = convert_chat_history_to_serial(st.session_state.chat_history)
-
-    st.write(serial_chat_history)
-
-    chat_history_json = json.dumps(serial_chat_history, indent=4, default=default_handler)
+    serial_chat_hist = convert_chat_hist_to_serial(st.session_state.chat_hist)
+    chat_hist_json = json.dumps(serial_chat_hist, indent=4, default=default_handler)
 
     # Create a download button for the JSON file
     st.sidebar.download_button(
         label="Download: Chat History",
-        data=chat_history_json,
-        file_name="chat_history.json",
+        data=chat_hist_json,
+        file_name="chat_hist.json",
         mime="application/json",
     )
 
-    sql_queries = get_sql_queries_only(st.session_state.chat_history)
+    sql_queries = get_sql_queries_only(st.session_state.chat_hist)
     sql_queries_str = "\n".join(sql_queries)
 
     st.sidebar.download_button(
--- src/sql_8week_danny/sql_engine.py
+++ src/sql_8week_danny/sql_engine.py
@@ -1,30 +1,34 @@
 from pathlib import Path
 
 import duckdb
+import sqlglot
 from IPython.display import Markdown, display
 from loguru import logger
 
-
 LOG_FILE = "duckdb_db.log"
 
-logger.remove()  # Turn off console logging
+#logger.remove()  # Turn off console logging
 logger.add(LOG_FILE, format="{time:ddd d MMM YYYY - HH:mm:ss} {level} {message}", rotation="1 MB")
 
 
 class DuckDBEngine:
-    def __init__(self, db_path=None, rm_db=False):
+    def __init__(self, config=None, db_path=None, rm_db=False):
         """
         Initialise the DuckDB db.
         :param db_path: Path to the DuckDB database file. If None, an in-memory database is used.
         :param rm_db: Bool - remove db_path if it exists (default False)
         """
         self.table_names = []
+        if config:
+            self.config = config["sql"]
+        else:
+            self.config = None
         if db_path:
             if rm_db and Path(db_path).exists():
                 logger.info(f"Removing existing {db_path}")
                 logger.info(f"Removing existing {db_path}.wal")
-                Path(db_path).unlink()
-                Path(f"{db_path}.wal").unlink()
+                Path(db_path).unlink(missing_ok=True)
+                Path(f"{db_path}.wal").unlink(missing_ok=True)
 
             logger.info(f"Persisting {db_path}\n")
             self.connection = duckdb.connect(database=db_path)
@@ -58,6 +62,19 @@ class DuckDBEngine:
             self.table_names = [row[0] for row in result.fetchall()]
         except Exception as e:
             print(f"Error fetching table names: {e}")
+
+    def validate(self, query):
+        try:
+            expressions = sqlglot.parse(query, dialect=self.dialect)
+            if len(expressions) > 1:
+                raise ValueError("Only one statement is allowed")
+            disallowed_keywords = self.config["disallowed_keywords"]
+            for expression in expressions:
+                if any(token in expression.sql().upper() for token in disallowed_keywords):
+                    raise ValueError("Disallowed SQL keywords detected")
+            return True, "Query is valid"
+        except Exception as e:
+            return False, str(e)
 
     def query(self, sql, force_dataframe=False):
         """
